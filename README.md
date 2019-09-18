abc7aa8dc8330c1ddded7c3ecaedaefa91f47132
Andrew Seibert <seiberta@aetna.com>
2019-09-18 10:28:56 -0400

    initial upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# accordion-exercise
+Task: 
+
+1. Import the Google font Open Sans (https://fonts.google.com/specimen/Open+Sans) and replace the application font with it.
+2. Using the PDFs or PSD in the docs folder create a reusable accordion component that accepts title, content, and link data.
+3. Implement the component in the indicated area using the data provided in App.vue
+
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.svg" class="logo" alt="Aetna logo">
+    <div class="container">
+      <h1>Import your component here</h1>
+      <div v-for="(accordion, thing, index) in accordionData" :key="accordion.index">
+        <h2>{{ accordion.title }} - {{ accordion.index }}</h2>
+        <p>{{ accordion.content }}</p>
+        <a :href="accordion.link">{{ accordion.link }}</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      accordionData: [
+        {
+          title: 'Title 1',
+          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          link: 'http://www.google.com'
+        },
+        {
+          title: 'Title 2',
+          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          link: 'http://www.aetna.com'
+        },
+        {
+          title: 'Title 3',
+          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          link: 'http://www.yahoo.com'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  color: #414141;
+  margin-top: 20px;
+  h1 {
+    color: #7D3F98;
+  }
+  .logo {
+    width: 250px;
+  }
+}
+</style>
